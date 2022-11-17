29615b94d6b7d4d44cd3fdb1e789980db4757d1b
Jean Rodriguez <rodriguezzegarrajeanpierre@gmail.com>
2022-11-16 22:23:29 -0300

    add: Resolución de desafio2

--- DesafioDos.php
+++ DesafioDos.php
@@ -1,19 +1,21 @@
 <?php 
+header('Content-Type: application/json'); //Ayuda para darle mejor visibilidad al formato json.
 require_once 'Database.php';
 
 class DesafioDos {
 
-    public static function retriveLotes(int $loteID):void {
+    public static function retriveLotes(string $loteID):void { //Modificación del tipo de dato a string que sería el tipo correcto con relación a la base de datos.
 
         Database::setDB(); 
 
-        echo(json_encode(self::getLotes($loteID)));
+        echo json_encode(self::getLotes($loteID), JSON_PRETTY_PRINT);
     }
 
-    private static function getLotes (int $loteID){
+    private static function getLotes (string $loteID){ //Modificación del tipo de dato a string que sería el tipo correcto con relación a la base de datos.
         $lotes = [];
         $cnx = Database::getConnection();
-        $stmt = $cnx->query("SELECT * FROM debts WHERE lote = '$loteID' LIMIT 2");
+        $stmt = $cnx->query("SELECT * FROM debts WHERE lote = '$loteID'"); //Se removió el LIMIT de la consulta a la base, debido a que deseamos que nos traiga todos los registros relacionado a ese Id de Lote.
+        
         while($rows = $stmt->fetchArray(SQLITE3_ASSOC)){
             $lotes[] = (object) $rows;
         }
